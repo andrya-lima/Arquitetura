70edb1276f273caf20a1e7dbac4e6e526a5c0484
Ândrya Lima <andrya.rezende@gmail.com>
2017-05-30 19:23:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Arquitetura
+{
+    public class API
+    {
+        public static object HelloWorldLabel { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+                host.Run();
+        }
+    }
+}
